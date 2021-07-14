058c3e5414c00c74048f878ef0861f7b0130ff33
rex <rexhoney4@gmail.com>
2021-07-14 18:44:33 +0530

    nodemon to node

--- server.js
+++ server.js
@@ -20,15 +20,15 @@ app.listen(port,function()
 });
 var dbConfiObj =
 {
-    // host: "localhost",
-    // user: "root",
-    // password: "",
-    // database: "maindata"
+    host: "localhost",
+    user: "root",
+    password: "",
+    database: "maindata"
 
-    host: "us-cdbr-east-04.cleardb.com",
-    user: "b18b3ac4e2b587",
-    password: "011de83f",
-    database: "heroku_ea4bcb02340f567"
+    // host: "us-cdbr-east-04.cleardb.com",
+    // user: "b18b3ac4e2b587",
+    // password: "011de83f",
+    // database: "heroku_ea4bcb02340f567"
 }
 var dbcon = mysql.createConnection(dbConfiObj);
 dbcon.connect(function (err)
